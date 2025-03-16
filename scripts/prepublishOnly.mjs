edde28f85241d5dd4908190a4e787df660d6c2bb
Iwo Plaza <iwoplaza@gmail.com>
2025-03-16 22:28:17 +0100

    Verify proper tag when publishing

--- scripts/prepublishOnly.mjs
+++ scripts/prepublishOnly.mjs
@@ -1,3 +1,31 @@
+import { type } from "arktype";
+import { readPackageJSON } from "pkg-types";
+
+const PublishTag = type('"alpha" | "beta" | undefined');
+
+const packageJSON = await readPackageJSON();
+const chosenPublishTag = PublishTag.assert(process.env.npm_config_tag);
+
+let tagVerified = false;
+for (const { unit: tag } of PublishTag.json) {
+  if (packageJSON.version.includes(tag)) {
+    if (tag !== chosenPublishTag) {
+      throw new Error(
+        `Publishing under a mismatched tag "${chosenPublishTag}" for version ${packageJSON.version}. Use --tag ${tag}`,
+      );
+    }
+
+    tagVerified = true;
+    break;
+  }
+}
+
+if (!tagVerified) {
+  throw new Error(
+    `Publishing under a mismatched tag "${chosenPublishTag}" for version ${packageJSON.version}.`,
+  );
+}
+
 // This project uses pnpm-specific functionality, like extensions to publishConfig in package.json.
 if (!process.env.npm_config_user_agent.startsWith("pnpm")) {
   console.error(
